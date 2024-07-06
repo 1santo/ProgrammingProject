c4291f67c3ee75c34c99869e408e27162d9ee4e1
1santo <tsvde@iscte-iul.pt>
2024-07-06 16:06:00 +0200

    Obstacles movem coordenadamente

--- Projeto2023Snake2SemestreBase/Projeto2023Snake2SemestreBase/src/game/ObstacleMover.java
+++ Projeto2023Snake2SemestreBase/Projeto2023Snake2SemestreBase/src/game/ObstacleMover.java
@@ -45,34 +45,58 @@ public class ObstacleMover extends Thread {
 	
 	//
 	protected void move(Cell toCell) throws InterruptedException {
+		try {
+			Cell lastCell =obstacle.getCells().getLast();
+			
+			Cell smallest = lastCell.compareTo(toCell)<0 ? lastCell : toCell; //condition ? value_if_true : value_if_false;
+			Cell biggest = lastCell.compareTo(toCell)<0 ? toCell : lastCell;
+			
+			smallest.getLock().lock();
+			try {
+				biggest.getLock().lock();
+					try {
+						System.out.println("1. "+obstacle+" na pos: "+obstacle.getCells());
+						Cell last =obstacle.getCells().getLast();
+						last.removeObstacle();
+						System.out.println("1234567890TESTINHO");
+						toCell.setGameElement(obstacle);
+						//	addObstacles(obs);
+							//addGameElement(obs);
+						//}
+						
+						
+						// clear obstacle list , necessary when resetting obstacles.
+						System.out.println("2.Obstacle na pos: "+obstacle.getCells());
+						board.getObstacles().clear();
+						
+						
+						//board.getObstacles().clear();
+					//	board.removeGameElement(this.obstacle);
+				
+						
+						board.setChanged();
+				
+						
+						System.out.println("cleared, lista obst: "+board.getObstacles());		
+				
+						
+					    		//	this.obstacle.doInitialPositioning();
+					    			System.out.println("obst"+this.obstacle);
+					    			board.setChanged();
 		
-		System.out.println("1.Obstacle na pos: "+obstacle.getCells());
-		Cell last =obstacle.getCells().getLast();
-		last.removeObstacle();
-		toCell.setGameElement(obstacle);
-		//	addObstacles(obs);
-			//addGameElement(obs);
-		//}
+	    			
+	    			
+					}finally {
+						biggest.getLock().unlock();
+					}
+					
+			}finally {
+				smallest.getLock().unlock();
+			}
 		
+			}catch (NullPointerException e) {
+		System.out.println("wait till snake has head?");
 		
-		// clear obstacle list , necessary when resetting obstacles.
-		System.out.println("2.Obstacle na pos: "+obstacle.getCells());
-		board.getObstacles().clear();
-		
-		
-		//board.getObstacles().clear();
-	//	board.removeGameElement(this.obstacle);
-
-		
-		board.setChanged();
-
-		
-		System.out.println("cleared, lista obst: "+board.getObstacles());		
-
-		
-	    		//	this.obstacle.doInitialPositioning();
-	    			System.out.println("obst"+this.obstacle);
-	    			board.setChanged();
-		
+		}
 		}
 }
